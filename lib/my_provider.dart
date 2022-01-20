a569c823e2bf3d3fd474c6f08134eb585ffd4ed9
shady <shady.samara@gmail.com>
2022-01-20 15:40:42 +0200

    selector

--- lib/my_provider.dart
+++ lib/my_provider.dart
@@ -2,6 +2,15 @@ import 'package:flutter/material.dart';
 
 class MyProvider extends ChangeNotifier {
   String name = 'omar';
+  String country = 'palestine';
+  String city = 'gaza';
+  String area = 'isalmic university';
+  bool acceptConditions = false;
+  changeAcceptConditions() {
+    acceptConditions = !acceptConditions;
+    notifyListeners();
+  }
+
   changeName(String newName) {
     this.name = newName;
     notifyListeners();
--- lib/provider_screen_test.dart
+++ lib/provider_screen_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project3/auth_provider.dart';
 import 'package:project3/my_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -7,22 +8,17 @@ class ProviderScreenTest extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            Provider.of<MyProvider>(context).name,
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 20),
-          ),
-          RaisedButton(onPressed: () {
-            Provider.of<MyProvider>(context, listen: false)
-                .changeName('hello world');
-          })
-        ],
-      ),
-    );
+        appBar: AppBar(),
+        body: Selector<MyProvider, bool>(builder: (context, acceptnaceBool, x) {
+          return CheckboxListTile(
+              title: Text('Accept Conditions'),
+              value: acceptnaceBool,
+              onChanged: (value) {
+                Provider.of<MyProvider>(context, listen: false)
+                    .changeAcceptConditions();
+              });
+        }, selector: (context, provider) {
+          return provider.acceptConditions;
+        }));
   }
 }
